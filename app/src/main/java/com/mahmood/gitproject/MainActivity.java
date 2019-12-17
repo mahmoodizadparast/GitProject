97b505232542a2847c48e83387e07b85448bc19b
mahmood <m.izadparast@royalco.ir>
2019-12-17 12:11:15 +0330

    final 2

--- app/src/main/java/com/mahmood/gitproject/MainActivity.java
+++ app/src/main/java/com/mahmood/gitproject/MainActivity.java
@@ -3,13 +3,29 @@ package com.mahmood.gitproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //ergergerg
+
+        FrameLayout fr = new FrameLayout(this);
+        fr.setBackgroundColor(getResources().getColor(R.color.color1));
+        final Button btn =new Button(this);
+        btn.setText("Clicke Me");
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("Click shod");
+            }
+        });
+        fr.addView(btn);
+        setContentView(fr);
+
+       // setContentView(R.layout.activity_main);
     }
 }
